834f5e5ccd940d2b559f4683ba86262935e276d0
bida24-338 <bida24-338@thuto.bac.ac.bw>
2025-05-24 18:48:48 +0200

    Delete Nike directory

--- Nike/Feedjava.java
+++
@@ -1,9 +0,0 @@
-
-const stars = document.querySelectorAll('.stars i');
-const card = document.querySelector('.card');
-
-stars.forEach(function(star) {
-    star.addEventListener('mouseover', function() {
-        card.className = `card ${this.id}`
-    })
-});
